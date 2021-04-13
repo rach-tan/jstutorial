82560e789ad801bfda0acee609e962349e6c1492
Rachelle Tan <rachelle.tan@kmart.com.au>
2021-04-13 14:10:46 +1000

    Update numbers-max.ts

--- src/numbers-max.ts
+++ src/numbers-max.ts
@@ -1,8 +1,6 @@
 //const max = 0;
 //const numberArray = [4, 1, 7, 2, 13, 54, 98, 32, 1, 23, 54];
-const checkMax = (max: number, inputNum: number) => {
-    return (inputNum > max) ? inputNum : max;
-};
+const checkMax = (max: number, inputNum: number) => (inputNum > max) ? inputNum : max;
 
 export const numbersMax = (numberArray: number[]) => {
     return numberArray.reduce(checkMax, 0)
