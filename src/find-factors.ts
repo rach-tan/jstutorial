d083eb470b5334b1767cfd257c50eced4d50b8dc
Rachelle Tan <rachelle.tan@kmart.com.au>
2021-04-13 14:20:52 +1000

    Update find-factors.ts

--- src/find-factors.ts
+++ src/find-factors.ts
@@ -1,14 +1,6 @@
-const max = 0;
-const numberArray = [2, 3, 4, 6, 9, 10, 11];
-const factor = 3;
-function checkFactor (inputNum) {
-    return (inputNum % factor) == 0
-};
-
-const findfactors = () => {
+export const findfactors = (numberArray: number[], factor: number) => {
+    const checkFactor = (inputNum: number) => {
+        return (inputNum % factor) == 0
+    }
     return numberArray.filter(checkFactor)
 };
-
-console.log(numbersMax);
-
-//export numbersMax;
